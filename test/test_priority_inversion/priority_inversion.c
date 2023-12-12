bf8e976a9bb407512d16ddb9282a09a44f5ecb24
Fredi R. Mino <35937021+Fredware@users.noreply.github.com>
2023-12-12 13:09:38 -0700

    Test file compiles succesfully. Need to verify that test is able to run.

--- test/test_priority_inversion/priority_inversion.c
+++ test/test_priority_inversion/priority_inversion.c
@@ -3,11 +3,11 @@
 #include <arch/cpu.h>
 #include <unity.h>
 
-#define STACKSIZE 2000;
+#define STACKSIZE 2000
 
+K_THREAD_STACK_DEFINE(super_stack, STACKSIZE);
 K_THREAD_STACK_DEFINE(primary_stack, STACKSIZE);
 K_THREAD_STACK_DEFINE(secondary_stack, STACKSIZE);
-K_THREAD_STACK_DEFINE(super_stack, STACKSIZE);
 
 struct k_thread primary_thread, secondary_thread, super_thread;
 struct k_sem semaphore;
@@ -18,17 +18,31 @@ uint64_t start, primary, secondary, end, elapsed;
 k_thread_runtime_stats_t pri_stats, sec_stats, start_stats, end_stats;
 
 
-void super_entry(struct k_thread *primary_thread, struct k_thread *secondary_thread){
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void super_entry(struct k_thread *primary_thread, struct k_thread *secondary_thread)
+{
     k_thread_suspend(primary_thread);
     k_thread_suspend(secondary_thread);
-
 }
 
-k_sem_init(&semaphore,0,1);
+void primary_entry(struct k_sem semaphore)
+{
+    //higher priority
+    k_sem_take(&semaphore, K_FOREVER);
+}
 
-void test_prio_inv(void){
-    
+void secondary_entry(struct k_sem semaphore)
+{
+    //lower priority
+    k_sem_take(&semaphore, K_FOREVER);
+}
 
+void test_priority_inversion(void)
+{
+    k_sem_init(&semaphore,0,1);
     k_thread_runtime_stats_all_get(&start_stats);
     // Meta-IRQ is enabled, forcing this to always preempt.
     k_thread_create(&super_thread, 
@@ -39,29 +53,34 @@ void test_prio_inv(void){
                     &secondary_thread,
                     NULL,
                     super_prio,
-                    K_SEC(100)
-                    )
+                    0,
+                    K_MSEC(10000)
+                    );
 
     k_thread_create(&primary_thread, 
                     primary_stack, 
                     STACKSIZE, 
-                    (k_thread_entry_t) pri_entry,
+                    (k_thread_entry_t) primary_entry,
                     &semaphore,
                     NULL,
                     NULL,
-                    1,
-                    K_SEC(1000)
+                    K_PRIO_PREEMPT(0),
+                    0,
+                    K_MSEC(1000)
                     );
+
     k_thread_create(&secondary_thread, 
                     secondary_stack, 
                     STACKSIZE, 
-                    (k_thread_entry_t) sec_entry,
+                    (k_thread_entry_t) secondary_entry,
                     &semaphore,
                     NULL,
                     NULL,
+                    K_PRIO_PREEMPT(1),
                     0,
-                    K_SEC(500)
+                    K_MSEC(500)
                     );
+
     k_thread_join(&super_thread, K_MSEC(5500));
 
     k_thread_runtime_stats_get(&primary_thread, &pri_stats);
@@ -74,28 +93,17 @@ void test_prio_inv(void){
     end = timing_cycles_to_ns(end_stats.execution_cycles) / 1000;
     elapsed = end - start;
 
-
     k_thread_abort(&primary_thread);
     k_thread_abort(&secondary_thread);
 
-
+    TEST_ASSERT_UINT64_WITHIN(1000, 0, primary);
 }
 
-void pri_entry(struct k_sem semaphore){//higher priority
-    
-    k_sem_take(&semaphore);
-
-}
-void sec_entry(struct k_sem semaphore){//lower priority
-    
-    k_sem_take(&semaphore);
-
-}
-
-int main(void){
+int main(void)
+{
     UNITY_BEGIN();
 
-    RUN_TEST(test_prio_inv);
+    RUN_TEST(test_priority_inversion);
 
     return UNITY_END();
 }
